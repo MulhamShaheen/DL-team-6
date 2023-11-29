4a5023a83493e0fd90967c1d6909e1bd92ceb346
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-11-29 18:18:10 +0300

    Reformed old version for videos

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'traffic_signs',
+    'traffic_signs.apps.TrafficSignsConfig',
 ]
 
 MIDDLEWARE = [
