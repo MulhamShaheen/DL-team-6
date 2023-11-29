03ba09c3bc5ae7d9733b3c12dbee84397cca4eff
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-11-29 17:54:08 +0300

    Added video rendering and saving

--- inference_api/infrustructure/detector.py
+++ inference_api/infrustructure/detector.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
-from PIL import Image
-from torchvision.transforms import functional as F
+import os
+import cv2
 from typing import List
 from dataclasses import dataclass, asdict
 import logging
 import numpy as np
 from .contour import Contour
 from ultralytics import YOLO
+from cap_from_youtube import cap_from_youtube
+
 
 
 @dataclass
@@ -17,12 +19,21 @@ class DetectorPrediction:
 
     dict = asdict
     names = {
-        0: 'missing_hole',
-        1: 'mouse_bite',
-        2: 'open_circuit',
-        3: 'short',
-        4: 'spurious_copper',
-        5: 'spur'
+        0: 'blue_border',
+        1: 'blue_rect',
+        2: 'danger',
+        3: 'main_road',
+        4: 'mandatory',
+        5: 'prohibitory'
+    }
+
+    colores = {
+        0: (255, 0, 0),
+        1: (255, 122, 0),
+        2: (0, 255, 0),
+        3: (0, 255, 122),
+        4: (0, 0, 255),
+        5: (122, 0, 255)
     }
 
     def dict(self):
@@ -30,6 +41,7 @@ class DetectorPrediction:
             "contour": self.predicted_contour.xyxy,
             "probability": self.contour_probability,
             "class": self.names[int(self.contour_class)],
+            "color": self.colores[int(self.contour_class)],
         }
 
 
@@ -61,7 +73,7 @@ class DummyDetector(Detector):
         return [prediction]
 
 
-class YoloV5Detector(DummyDetector):
+class YoloDetector(DummyDetector):
 
     def __init__(self, detection_model_path: str):
         super().__init__(detection_model_path)
@@ -70,10 +82,9 @@ class YoloV5Detector(DummyDetector):
     def _load_model(self):
         self.model = YOLO(self.model_path)
 
-    def detect_contours(self, img_path: str):
+    def detect_contours(self, img) -> [DetectorPrediction]:
         predictions = []
-        img = Image.open(img_path)
-        results = self.model.predict(img)
+        results = self.model(img)
         for result in results[0].boxes.data:
             # data = result.boxes.data[0]
 
@@ -82,6 +93,43 @@ class YoloV5Detector(DummyDetector):
             predictions.append(prediction)
 
         return predictions
+
+    def detect_contours_video(self, vid_url: str, save=True):
+        video_id = vid_url.split("/")[-2] + "_" + vid_url.split("/")[-1]
+        frames_dir = f"media/frames/{video_id}"
+        video_dir = "media/videos/"
+        os.makedirs(frames_dir,exist_ok=True)
+        cap = cap_from_youtube(vid_url, resolution='720p')
+        start_time = 0
+        cap.set(cv2.CAP_PROP_POS_FRAMES, start_time * cap.get(cv2.CAP_PROP_FPS))
+        count = 0
+        height, width = 0, 0
+        res = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            height, width, layers = frame.shape
+            preds = self.detect_contours(frame)
+            res.append(preds)
+            for pred in preds:
+                data = pred.dict()
+                cv2.rectangle(frame, (int(pred.predicted_contour.x_min), int(pred.predicted_contour.y_min)),
+                              (int(pred.predicted_contour.x_max), int(pred.predicted_contour.y_max)), data["color"], 2)
+
+            cv2.imwrite(f"{frames_dir}/{count}.jpg", frame)
+            count += 1
+
+        if save:
+            video = cv2.VideoWriter(video_id+".avi", 0, 1, (width, height))
+            images = [img for img in os.listdir(frames_dir) if img.endswith(".jpg")]
+            for image in images:
+                video.write(cv2.imread(os.path.join(frames_dir, image)))
+
+            cv2.destroyAllWindows()
+            video.release()
+
+        return res, video_dir + video_id
 
     def save_predictions(self, img_path: str, path: str):
         predictions = self.model(img_path)
